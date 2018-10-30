e98c2fb4f2f78606a20be38fee1a5949cd821b74
Nathan Vander Wilt <nate@calftrail.com>
2018-10-29 19:09:02 -0700

    more utterance tweaks

--- skill/generate.js
+++ skill/generate.js
@@ -4,7 +4,7 @@ function* expand(utterance) {
     if (typeof group === 'string') plainWords.push(group);
     else {
       // (assume) array needing expansion
-      if (group.length === 1) group = group.concat('')
+      if (group.length < 2) group = group.concat('')
       for (let option of group) {
         let filledUtterance = (option === '') ?
           [...plainWords, ...utterance.slice(plainWords.length+1)] :
@@ -29,23 +29,24 @@ function output(utterances) {
   }
 }
 
-const EGGS = ["egg", "eggs", "more egg", "more eggs"],
+const EGGS = ["egg", "eggs", "more egg", "more eggs"], TODAY = [], // TODAY = ["today", "now", "just now", ''],
       MY = ["my", "our"], WE = ["I", "we"], COUNT = "{count}";
 
 output([
-  [["record"], COUNT, EGGS, ["laid"]],
-  [MY, ["chicken", "chickens", "hen", "hens"], "laid", COUNT, EGGS],
-  [WE, ["got", "found", "gathered"], COUNT, EGGS],
+  [["record"], COUNT, EGGS, ["laid","found","gathered","collected",''], TODAY],
+  [MY, ["chicken", "chickens", "hen", "hens"], "laid", COUNT, EGGS, TODAY],
+  [WE, ["got", "found", "gathered"], COUNT, EGGS, TODAY],
 ]);
 
 console.log("---")
 
 const TELL = ["tell me", "tell", ''];  //.concat("show", "show me");
 output([
-  [TELL, "how many eggs have we", ["got", "gotten"]],
+  [TELL, "how many eggs have we", ["got", "gotten", "gathered", "found", "collected"]],
   [TELL, "how many eggs have the", ["hens","chickens"], "laid"],
   [TELL, "how many eggs the", ["hens","chickens"], "have laid"],
   [TELL, "how many eggs has the", ["hen","chicken"], "laid"],
   [TELL, "how many eggs the", ["hen","chicken"], "has laid"],
-  [["tell", "tell me", "what's", "what is"], "the egg count"]
+  [["tell", "tell me", "what's", "what is"], "the egg count"],
+  [TELL, "how many eggs"]
 ]);
